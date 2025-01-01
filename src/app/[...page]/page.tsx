691ce013355683474a2f5d5833d484d896e2ace1
hoanggia7398 <44608643+hoanggia7398@users.noreply.github.com>
2025-01-01 10:31:01 +0700

    1st init

---
+++ src/app/[...page]/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { builder } from "@builder.io/sdk";
+import { RenderBuilderContent } from "../../components/builder";
+import { useEffect, useState } from "react";
+
+// Builder Public API Key set in .env file
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+
+interface PageProps {
+  params: Promise<{
+    page: string[];
+  }>;
+}
+
+export default function Page(props: PageProps) {
+  const builderModelName = "page";
+  const [content, setContent] = useState(null);
+
+  useEffect(() => {
+    async function fetchContent() {
+      const resolvedParams = await props.params;
+      const content = await builder
+        .get(builderModelName, {
+          userAttributes: {
+            urlPath: "/" + (resolvedParams?.page?.join("/") || ""),
+          },
+        })
+        .toPromise();
+      setContent(content);
+    }
+    fetchContent();
+  }, [props.params]);
+
+  return (
+    <>
+      {content && (
+        <RenderBuilderContent content={content} model={builderModelName} />
+      )}
+    </>
+  );
+}
